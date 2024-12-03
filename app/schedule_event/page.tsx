60da57f71e066d0db10d6a9aa8abd767a831a6ba
lapotts1 <lapotts1@cs.olemiss.edu>
2024-12-03 11:55:16 -0600

    event

--- app/schedule_event/page.tsx
+++ app/schedule_event/page.tsx
@@ -1,4 +1,3 @@
-// schedule_event/page.tsx
 "use client";
 
 import React, { useState } from "react";
@@ -20,21 +19,14 @@ export default function ScheduleEvent() {
     roomNumber: "",
   });
 
-<<<<<<< HEAD
   const router = useRouter();
 
-=======
->>>>>>> eeaefa06f6fe689e5f486537a4347f285b9c32a1
   const today = new Date();
   const minDate = new Date(today);
   minDate.setDate(today.getDate() + 2); // Two days ahead
 
   const formatDate = (date: Date) => {
-<<<<<<< HEAD
     return date.toISOString().split("T")[0];
-=======
-    return date.toISOString().split('T')[0];
->>>>>>> eeaefa06f6fe689e5f486537a4347f285b9c32a1
   };
 
   const handleInputChange = (
@@ -43,11 +35,7 @@ export default function ScheduleEvent() {
     const { name, value } = e.target;
 
     // Validate room number to only accept positive integers
-<<<<<<< HEAD
     if (name === "roomNumber" && value !== "") {
-=======
-    if (name === 'roomNumber' && value !== '') {
->>>>>>> eeaefa06f6fe689e5f486537a4347f285b9c32a1
       const isValid = /^\d+$/.test(value);
       if (!isValid) return; // Ignore invalid input
     }
@@ -60,7 +48,6 @@ export default function ScheduleEvent() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-<<<<<<< HEAD
 
     if (!user || !user.uid) {
       console.error("User not logged in");
@@ -87,10 +74,6 @@ export default function ScheduleEvent() {
     } catch (error) {
       console.error("Error scheduling event:", error);
     }
-=======
-    console.log('Event Scheduled:', formData);
-    // Add form submission logic here
->>>>>>> eeaefa06f6fe689e5f486537a4347f285b9c32a1
   };
 
   return (
@@ -126,7 +109,6 @@ export default function ScheduleEvent() {
               required
               min={formatDate(minDate)} // Restrict minimum date
               className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              min={formatDate(minDate)} // Restrict minimum date
             />
           </div>
           <div>
@@ -144,13 +126,9 @@ export default function ScheduleEvent() {
             />
           </div>
           <div>
-<<<<<<< HEAD
             <label htmlFor="buildingName" className="block text-lg font-medium">
               Building Name
             </label>
-=======
-            <label htmlFor="buildingName" className="block text-lg font-medium">Building Name</label>
->>>>>>> eeaefa06f6fe689e5f486537a4347f285b9c32a1
             <select
               id="buildingName"
               name="buildingName"
@@ -206,3 +184,4 @@ export default function ScheduleEvent() {
     </>
   );
 }
+
